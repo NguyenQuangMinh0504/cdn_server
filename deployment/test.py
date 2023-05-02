b21a3ff0b02e3f8919b6c0f7fe3f1c55c3e883d2
Nguyen Quang Minh <ngquangminh05042001@gmail.com>
2023-05-02 16:21:33 +0700

    .

--- deployment/test.py
+++ deployment/test.py
@@ -1,6 +1,6 @@
 from redis import Redis
 import json
-redis_table = Redis(host="127.0.0.1", port=6379, db=1)
+redis_table = Redis(host="34.28.133.140", port=6379, db=1)
 print(redis_table.keys())
 redis_table.set("saugau.com",
                 json.dumps({"host": "saugau.com", "ip": "104.154.99.60:80"}))
