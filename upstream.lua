3ec168d45e866140dc0eb656dab711ae66286efa
Nguyen Quang Minh <ngquangminh05042001@gmail.com>
2023-05-20 11:03:08 +0700

    .

--- upstream.lua
+++ upstream.lua
@@ -5,15 +5,8 @@ local red = redis:new()
 host = ngx.var.host
 
 red:connect("127.0.0.1", 6380)
-red:select(1)
-ngx.log(ngx.ERR, "Host is " .. host)
+red:select(0)
 local res, err = red:get(host)
-ngx.log(ngx.ERR, "Res is " .. res)
-local config = json.decode(res)
-ngx.var.x_upstream_addr = config["ip"]
-ngx.var.x_host = config["host"]
-local h = ngx.req.get_headers()
-  for k, v in pairs(h) do
-    ngx.log(ngx.ERR, "Got header "..k..": "..tostring(v)..";")
-  end
+ngx.var.x_upstream_addr = res
+ngx.var.x_host = res
 ngx.log(ngx.ERR, ngx.var.x_upstream_addr)
