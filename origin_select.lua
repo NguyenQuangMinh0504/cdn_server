d37562ea757927a40090008d645153d68b411a6c
Nguyen Quang Minh <ngquangminh05042001@gmail.com>
2023-06-10 10:38:23 +0700

    .

--- origin_select.lua
+++ origin_select.lua
@@ -3,10 +3,15 @@ local json = require "cjson"
 local red = redis:new()
 
 host = ngx.var.host
+request_uri = ngx.var.request_uri
 
 red:connect("127.0.0.1", 6380)
 red:select(0)
 local res, err = red:get(host)
 ngx.var.x_upstream_addr = res
 ngx.var.x_host = res
-ngx.log(ngx.ERR, ngx.var.x_upstream_addr)
+red_select(1)
+local res, err = red:get(host)
+if res == 1 then:
+    ngx.var.x_cache_key = host .. request_uri
+    
